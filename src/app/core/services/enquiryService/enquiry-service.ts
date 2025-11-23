20803164125aef862b95668c29015cd1eca819e7
AshwiniNikam1 <jashwini10@gmail.com>
2025-11-24 00:26:19 +0530

    fix: enquiry class modal and services are created

--- src/app/core/services/enquiryService/enquiry-service.ts
+++ src/app/core/services/enquiryService/enquiry-service.ts
@@ -1,8 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { APIConstants } from '../../constants/constant';
+import { EnquiryModal } from '../../modals/classModal/enquiryClass';
+import { Observable } from 'rxjs';
+import { Iapiresponse } from '../../modals/interfaceModal/apiResponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnquiryService {
-  
+  constructor(private http: HttpClient) {}
+
+  getAllQueries() {
+    return this.http.get(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.GET_ENQUIRIES
+    );
+  }
+
+  createEnquiry(payload: EnquiryModal): Observable<Iapiresponse> {
+    return this.http.post<Iapiresponse>(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.CREATE_ENQUIRY,
+      payload
+    );
+  }
+
+  updateEnquiry(payload: EnquiryModal): Observable<Iapiresponse> {
+    return this.http.put<Iapiresponse>(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.UPDATE_ENQUIRY +
+        payload.statusId,
+      payload
+    );
+  }
+  deleteEnquiry(id: number): Observable<Iapiresponse> {
+    return this.http.delete<Iapiresponse>(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.DELETE_ENQUIRY +
+        id
+    );
+  }
+
+  getEnquiryById(id: number): Observable<Iapiresponse> {
+    return this.http.get<Iapiresponse>(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.GET_ENQUIRY_BY_ID +
+        id
+    );
+  }
+  filterEnquiries(filterParams: EnquiryModal): Observable<Iapiresponse> {
+    return this.http.post<Iapiresponse>(
+      environment.API_URL +
+        APIConstants.CONTROLLER_TYPES.ENQUIRY +
+        APIConstants.ENQUIRY.FILTER_ENQUIRIES,
+      filterParams
+    );
+  }
 }
